d3eb3f72a8ac6a469f90d140281fbef48bfa006e
scjtqs <scjtqs@qq.com>
2020-08-29 20:22:23 +0800

    fix bug

--- sed.php
+++ sed.php
@@ -23,9 +23,9 @@ $array['uin']=$qq;
 $array['password']=$password;
 $array['access_token']=$token;
 $array['http_config']['post_urls']=[$posturl=>$secret];
-$array['ws_reverse_servers']['enabled']=boolval($ws_enable);
-$array['ws_reverse_servers']['reverse_url']=$_SERVER['WS_REVERSE_URL'];
-$array['ws_reverse_servers']['reverse_api_url']=$_SERVER['WS_REVERSE_API_URL'];
-$array['ws_reverse_servers']['reverse_event_url']=$_SERVER['WS_REVERSE_EVENT_URL'];
+$array['ws_reverse_servers'][0]['enabled']=boolval($ws_enable);
+$array['ws_reverse_servers'][0]['reverse_url']=$_SERVER['WS_REVERSE_URL'];
+$array['ws_reverse_servers'][0]['reverse_api_url']=$_SERVER['WS_REVERSE_API_URL'];
+$array['ws_reverse_servers'][0]['reverse_event_url']=$_SERVER['WS_REVERSE_EVENT_URL'];
 
 file_put_contents($file_name,json_encode($array));
